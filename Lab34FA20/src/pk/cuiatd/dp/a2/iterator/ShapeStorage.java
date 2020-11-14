a05a4b2a56d1289023bb623f216f8f6d2223cbee
sajid <sajid.shah.de@gmail.com>
2020-11-14 11:19:33 +0500

    added the code for iterator DP

---
+++ Lab34FA20/src/pk/cuiatd/dp/a2/iterator/ShapeStorage.java
@@ -0,0 +1,13 @@
+package pk.cuiatd.dp.a2.iterator;
+
+public class ShapeStorage {
+	private Shape []shapes = new Shape[5];
+	private int index;
+	public void addShape(String name){
+		int i = index++;
+		shapes[i] = new Shape(i,name);
+	}
+	public Shape[] getShapes(){
+		return shapes;
+	}
+}
